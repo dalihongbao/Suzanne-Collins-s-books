6d12a6fa32c29bff71f05e155ef728deb9ad6ffb
Yifeng <evonnechan@live.com>
2020-09-03 16:28:37 +0930

    Uploaded Suzanne-Collins Project to master

---
+++ Assignment1/Assignment1/Dynamic.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Assignment1.Models;
+
+namespace Assignment1
+{
+    public partial class Dynamic : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var db = new SuzanneEntities())
+            {
+                var items = db.Books
+                    .OrderBy(b => b.Id)
+                    .Select(b => b)
+                    .ToList();
+
+
+                RBooks.DataSource = items;
+
+                RBooks.DataBind();
+            }
+        }
+
+        
+    }
+}
